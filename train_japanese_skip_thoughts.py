ca2bec869cf580f6635561479327ff5a00467c22
philipperemy <premy@reactive.co.jp>
2016-09-05 18:21:37 +0900

    refactored

--- train_japanese_skip_thoughts.py
+++ train_japanese_skip_thoughts.py
@@ -1,19 +1,22 @@
 from __future__ import print_function
 
-import os
+from gensim.models import Word2Vec as word2vec
 
 import skipthoughts.training.tools as st_tools
 import skipthoughts.training.train as st_train
 import skipthoughts.training.vocab as st_vocab
 
+
+def load_w2v_vectors(path_to_w2v):
+    return word2vec.load_word2vec_format(path_to_w2v, binary=False)
+
+
 if __name__ == '__main__':
-    if not os.path.exists('tmp'):
-        os.makedirs('tmp')
     X = ['cat is on the floor.'] * 100
     word_dict, wordcount = st_vocab.build_dictionary(X)
-    st_vocab.save_dictionary(word_dict, wordcount, 'tmp/dict.dat')
-    st_train.trainer(X, saveto='tmp/toy.npz', dictionary='tmp/dict.dat')
-    embed_map = st_tools.load_googlenews_vectors()
+    st_vocab.save_dictionary(word_dict, wordcount, 'dict.dat')
+    st_train.trainer(X, saveto='toy.npz', dictionary='dict.dat')
+    embed_map = load_w2v_vectors('ja-gensim.50d.data.txt')
     model = st_tools.load_model(embed_map)
     vectors = st_tools.encode(model, X)
     print(vectors)
