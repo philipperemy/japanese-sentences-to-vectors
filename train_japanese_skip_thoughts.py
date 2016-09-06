090fde267d97352df36e5b6849e669887b9e1c07
philipperemy <premy@reactive.co.jp>
2016-09-06 11:00:07 +0900

    refactored

--- train_japanese_skip_thoughts.py
+++ train_japanese_skip_thoughts.py
@@ -5,6 +5,10 @@ from gensim.models import Word2Vec as word2vec
 import skipthoughts.training.tools as st_tools
 import skipthoughts.training.train as st_train
 import skipthoughts.training.vocab as st_vocab
+import re
+
+SENTENCES_FILENAME = 'jawiki-latest-text-sentences-tokens.txt'
+#SENTENCES_FILENAME = 'test.txt'
 
 
 def load_w2v_vectors(path_to_w2v):
@@ -12,11 +16,17 @@ def load_w2v_vectors(path_to_w2v):
 
 
 if __name__ == '__main__':
-    X = ['cat is on the floor.'] * 100
-    word_dict, wordcount = st_vocab.build_dictionary(X)
-    st_vocab.save_dictionary(word_dict, wordcount, 'dict.dat')
-    st_train.trainer(X, saveto='toy.npz', dictionary='dict.dat')
-    embed_map = load_w2v_vectors('ja-gensim.50d.data.txt')
-    model = st_tools.load_model(embed_map)
-    vectors = st_tools.encode(model, X)
-    print(vectors)
+    with open(SENTENCES_FILENAME, 'r') as inp:
+        X = []
+        for i, line in enumerate(inp.readlines()):
+            if not re.match('[a-zA-Z]+', line):
+                if i % 1000 == 0:
+                    print('Size of the sentence matrix X = {}'.format(len(X)))
+                X.append(line.strip())
+        word_dict, wordcount = st_vocab.build_dictionary(X)
+        st_vocab.save_dictionary(word_dict, wordcount, 'dict.dat')
+        st_train.trainer(X, saveto='toy.npz', dictionary='dict.dat')
+        embed_map = load_w2v_vectors('ja-gensim.50d.data.txt')
+        model = st_tools.load_model(embed_map)
+        vectors = st_tools.encode(model, X)
+        print(vectors)
